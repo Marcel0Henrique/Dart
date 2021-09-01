6e4b897d2ee76e4e7a84769000ae7d7cd8e9e3a6
Marcel0Henrique <marcelomonteiro1275@gmail.com>
2021-09-01 15:58:44 -0300

    Update 01/07/21

---
+++ 01_algoritmos_computacionais/ Concatenação_de_texto .dart
@@ -0,0 +1,16 @@
+main(List<String> args) {
+  //*Concatenação de texto*
+
+  //concatenação dentro de variaveis
+  var texto = "Hello " + "World"; //Exemplo 1
+
+  //concatenação com as variaveis
+  String nome = "Marcelo ";
+  String sobrenome = "Monteiro"; //Exemplo 2
+
+  //Exemplo 1
+  print(texto);
+
+  //Exemplo2
+  print(nome + sobrenome);
+}
