7dcc61f520bb121c8d241d23cf1148e83ee97653
Marcel0Henrique <marcelomonteiro1275@gmail.com>
2021-09-09 11:35:31 -0300

    Laço For

---
+++ 03_sintaxes/02_loop_com_for.dart
@@ -0,0 +1,12 @@
+main(List<String> args) {
+  //*Laço de repetição FOR
+  /*
+    O laço for é uma estrutura de repetição muito utilizada nos programas.
+    É muito útil quando se sabe de antemão quantas vezes a repetição deverá ser executada. 
+    Este laço utiliza uma variável para controlar a contagem do loop, bem como seu incremento.
+  */
+
+  for (int i = 0; i < 10; i++) {
+    print("Rodou: ${i}");
+  }
+}
