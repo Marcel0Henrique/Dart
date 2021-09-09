643f6621964e957acbd53b76ee77bd889f837a24
Marcel0Henrique <marcelomonteiro1275@gmail.com>
2021-09-09 15:21:19 -0300

    Laço Do/While

--- 03_sintaxes/04_do_while.dart
+++ 03_sintaxes/04_do_while.dart
@@ -1,1 +1,13 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  //* Laço de repetição Do/While
+  /**
+   * O do/while tem quase o mesmo funcionamento que o while, 
+   * a diferença é que com o uso dele teremos os comandos executados ao menos uma única vez.
+   */
+
+  int n = 0;
+  do {
+    print("O valor de n é: ${n}");
+    n++;
+  } while (n != 10);
+}
