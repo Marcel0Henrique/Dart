3cad1ab9e35502f58f6b945ca7e11769ac3e4577
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2021-11-25 19:46:57 -0300

    update

--- 000 - Primeiro projeto em Flutter/contador/lib/main.dart
+++ 000 - Primeiro projeto em Flutter/contador/lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Contador'),
     );
   }
 }
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Quantidade de vezes que você apertou o botão:',
             ),
             Text(
               '$_counter',
