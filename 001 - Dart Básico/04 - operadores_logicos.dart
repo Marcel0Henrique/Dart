70e7b4e7a0920cdbb5086181115ca3ebff29bfb7
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2021-11-26 12:55:14 -0300

    Create 04 - operadores_logicos.dart

---
+++ 001 - Dart Básico/04 - operadores_logicos.dart
@@ -0,0 +1,27 @@
+void main(List<String> args) {
+  //* Operadores Logicos
+  /*  
+
+    Maior que | >
+    Maior ou igual | >=
+
+    Menor que | <
+    Menor ou igual | <=
+
+    Igual | ==
+    Diferente | !=
+
+  */
+
+  /*
+    AND(E) | &
+    OR(OU) | |
+  */
+
+  print(15 > 10);
+  print(15 >= 15);
+  print(30 < 20);
+  print(35 <= 35);
+  print(5 == 5);
+  print(75 != 100);
+}
