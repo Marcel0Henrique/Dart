188f90cd6aa4262d1214cb4a5475f1f6e8722ea2
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2021-11-25 21:57:12 -0300

    update

---
+++ 001 - Dart Básico/01 - variaveis_basicas.dart
@@ -0,0 +1,37 @@
+void main(List<String> args) {
+  //? Tipos de variaveis
+  //* Numéricos:
+    /*
+      int – inteiros
+      double – decimais
+    */
+
+  //* Texto
+    /*
+      String - texto
+    */
+
+  //* Logico
+    /*
+      bool - verdadeiro ou falso
+    */
+
+  //* Dinamico
+    /*
+      var - pode ser de qualquer tipo, permite mudar o tipo adicionando novos valores
+
+      dynamic - pode ser de qualquer tipo, o primeiro valor que receber vai definir o tipo e não muda
+    */
+
+
+
+  //? Declarando variaveis
+
+  int inteiro = 10;
+  double decimal = 7.85;
+  String texto = "Olá Mundo!";
+  bool logico_verdadeiro = true;
+  bool 
+
+  print(decimal);
+}
