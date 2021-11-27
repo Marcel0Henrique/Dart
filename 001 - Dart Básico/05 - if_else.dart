0639cbd364e1ea79e113f906a3777fed2c7c4438
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2021-11-27 00:46:37 -0300

    update

---
+++ 001 - Dart Básico/05 - if_else.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  double nota = 2;
+
+  if (nota < 6) {
+    print("Reprovado");
+  } else if (nota == 6) {
+    print("Aprovado Na Média");
+  } else {
+    print("Aprovado");
+  }
+}
---
+++ 001 - Dart Básico/06 -  operador_ternario.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  double nota = 5;
+  String mensagem = nota >= 6 ? 'Aprovado' : 'Reprovado';
+
+  print(mensagem);
+}
