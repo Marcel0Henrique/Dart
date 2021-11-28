4dac977f67eac5bb59e93443c40abd695a4f42b6
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2021-11-28 13:59:51 -0300

    switch

---
+++ 001 - Dart Básico/07 - switch.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  String linguagem = "java";
+
+  switch (linguagem) {
+    case "dart":
+      print("Linguagem Dart");
+      break;
+
+    case "python":
+      print("Linguagem Python");
+      break;
+
+    case "java":
+      print("Linguagem Java");
+      break;
+
+    default:
+      print("Não reconheci essa linguagem!");
+  }
+}
