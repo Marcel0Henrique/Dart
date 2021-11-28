82a5c548ca9bba72229eca6e8fb0b31d3d8f82b8
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2021-11-28 16:48:20 -0300

    update

--- 002 - Funções/02 - retorno_de_funcoes.dart
+++ 002 - Funções/02 - retorno_de_funcoes.dart
@@ -1,20 +1,23 @@
 void main(List<String> args) {
   saudacoes();
+  print('');
 
-  //? Armazena o retorno da função em uma variavel
-  String data = agora();
-
-  print(data);
+  print(soma(10, 15));
 }
 
 void saudacoes() {
   print("Olá meu amigo!");
+  print("A data atual é: ${data()}");
 }
 
 //* Acessa a data e hora atual e retorna ela
 //? o String define que vai retornar uma string no final
-String agora() {
-  DateTime agora = new DateTime.now();
+String data() {
+  DateTime data = new DateTime.now();
 
-  return agora.toString();
+  return data.toString();
+}
+
+int soma(int n1, int n2) {
+  return n1 + n2;
 }
