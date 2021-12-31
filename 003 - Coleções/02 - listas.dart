f6540e76f936b2f7dda1639c573994223c0e2c39
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2021-12-31 17:18:04 -0300

    Update 02 - listas.dart

--- 003 - Coleções/02 - listas.dart
+++ 003 - Coleções/02 - listas.dart
@@ -1,1 +1,15 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  //* Lista dinamica, aceita qualquer tipo de dado
+  List dinamica = ["Goku", "Kuririn", "Vegeta", "Gohan", 10, true];
+
+  //* Lista que aceita apenas String
+  List<String> nomes = ['Nappa', 'Trunks'];
+
+  //* Lista que aceita apenas Inteiros
+  List<int> idade = [10, 15, 65, 45];
+
+  //? Mostra a quantidade de itens dentro da lista
+  print(dinamica.length);
+  print(nomes.length);
+  print(idade.length);
+}
