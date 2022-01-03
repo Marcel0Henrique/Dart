6e2483381b18dac17488f14798c7d682d02e067b
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-01-03 16:58:42 -0300

    Update 04 - outras_funcoes_da_lista.dart

--- 003 - Coleções/04 - outras_funcoes_da_lista.dart
+++ 003 - Coleções/04 - outras_funcoes_da_lista.dart
@@ -1,5 +1,13 @@
 void main() {
   List<int> maluca = List.filled(100, 8);
 
+  List<int> doida = List.generate(10, (posicao) {
+    return posicao * 10;
+  });
+
   print(maluca);
+  print('');
+  print(doida);
+
+  print(doida.any((element) => false))
 }
