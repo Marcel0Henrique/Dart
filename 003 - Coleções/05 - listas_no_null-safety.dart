6ba3832200cbe9fd9b100a7483a8a9301bf72d16
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-01-04 18:11:18 -0300

    Update 05 - listas_no_null-safety.dart

--- 003 - Coleções/05 - listas_no_null-safety.dart
+++ 003 - Coleções/05 - listas_no_null-safety.dart
@@ -1,1 +1,23 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  //* Criando lista
+  List<String> nome = ['marcelo', 'henrique'];
+
+  //* Criando lista nula
+
+  //? aceita valores null dentro da lista
+  List<String?> dados = [null, 'jhon'];
+
+  //? cria uma lista vazia
+  List<String>? dados2;
+
+  if (dados2 != null) {
+    dados2.add('carlos');
+  }
+
+  //? Junção dos dois metodos
+  List<String?>? dados3;
+
+  if (dados3 != null) {
+    dados3.add(null);
+  }
+}
