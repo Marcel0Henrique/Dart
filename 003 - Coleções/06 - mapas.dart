05c0be643e1dab58eeb10fdcd0fa805a3f1ee3e3
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-01-04 20:46:33 -0300

    update

--- 003 - Coleções/06 - mapas.dart
+++ 003 - Coleções/06 - mapas.dart
@@ -1,1 +1,10 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  Map<int, String> DDDs = {11: 'são paulo', 81: 'Pernambuco', 41: 'Curitiba'};
+
+  print(DDDs[81]);
+  print(DDDs.containsKey(11));
+
+  DDDs[61] = 'Brasilia';
+
+  print(DDDs);
+}
--- exercicios/01 - gerador_perfil_naruto.dart
+++ exercicios/01 - gerador_perfil_naruto.dart
@@ -1,1 +1,7 @@
-void main(List<String> args) {}
+//* Esse pequeno exercicio usa lista para criar uma gerador de perfil de naruto
+
+void main(List<String> args) {
+  List<String> aldeias = ['Folha', 'Areia', 'Nevoa', 'Pedra', 'Nuvem'];
+  aldeias.sort(aldeias);
+  print(aldeias);
+}
