ac6689349565cb74803754c8149e367aa5d9a240
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-01-04 21:39:41 -0300

    Update 01 - gerador_perfil_naruto.dart

--- exercicios/01 - gerador_perfil_naruto.dart
+++ exercicios/01 - gerador_perfil_naruto.dart
@@ -8,12 +8,21 @@ void main(List<String> args) {
   String? nome;
   String? aldeia;
   String? clan;
+  String? nv_ninja;
 
   List<String> aldeias = ['Folha', 'Areia', 'Nevoa', 'Pedra', 'Nuvem'];
+  List<String> folha = ['Hyūga', 'Uchiha', 'Senju', 'Uzumaki', 'Hatake'];
   List<String> areia = ['Hōki', 'Kazekage', 'Shirogane'];
-  List<String> nevoa = [];
-  List<String> pedra = [];
-  List<String> nuvem = [];
+  List<String> nevoa = ['Hoshigaki', 'Hōzuki', 'Yuki', 'Karatachi'];
+  List<String> pedra = ['Kamizuru'];
+  List<String> nuvem = ['Yotsuki', 'Chinoike'];
+  Map<int, String> ninja = {
+    0: 'Genin',
+    1: 'Chūnin',
+    2: 'Tokubetsu Jōnin',
+    3: 'Jōnin',
+    4: 'Kage'
+  };
 
   //* Entrada de dados
   print("=" * 60);
@@ -22,27 +31,34 @@ void main(List<String> args) {
   print("=" * 60);
 
   //* Processamento
-  aldeia = aldeias[Random().nextInt(aldeias.length)].toUpperCase();
+  aldeia = aldeias[Random().nextInt(aldeias.length)];
 
   switch (aldeia) {
     case 'Folha':
+      clan = folha[Random().nextInt(folha.length)];
       break;
 
     case 'Areia':
+      clan = areia[Random().nextInt(areia.length)];
       break;
 
     case 'Nevoa':
+      clan = nevoa[Random().nextInt(nevoa.length)];
       break;
 
     case 'Pedra':
+      clan = pedra[Random().nextInt(pedra.length)];
       break;
 
     case 'Nuvem':
+      clan = nuvem[Random().nextInt(nuvem.length)];
       break;
 
     default:
       clan = 'Sem clan'.toUpperCase();
   }
+
+  nv_ninja = ninja[Random().nextInt(ninja.length)].toString().toUpperCase();
 
   //? Faz a aplicação dormir por 2 segundos
   sleep(const Duration(seconds: 2));
@@ -54,7 +70,10 @@ void main(List<String> args) {
   //* Saida de dados
   print("============" + " NINJA " + "============");
   print('');
-  print("Nome: ${nome + ' ' + aldeia}");
-  print("Aldeia: ${aldeia}");
-  print("Clan: ${clan}");
+  print("Nome: ${nome + ' ' + clan.toUpperCase()}");
+  print("Aldeia: ${aldeia.toUpperCase()}");
+  print("Clan: ${clan.toUpperCase()}");
+  print("Classificação Ninja: ${nv_ninja}");
+  print('');
+  print("===============================");
 }
