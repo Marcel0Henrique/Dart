a876462556ff9cbb9c2e7b7668adff1fd84b90d1
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-01-05 19:26:08 -0300

    Update 07 - funcoes_de_mapas_e_null_assertion.dart

--- 003 - Coleções/07 - funcoes_de_mapas_e_null_assertion.dart
+++ 003 - Coleções/07 - funcoes_de_mapas_e_null_assertion.dart
@@ -1,1 +1,19 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  Map<int, String> ddds = {
+    11: 'São Paulo',
+    81: 'Pernambuco',
+    19: 'Campinas',
+    41: 'Curitiba',
+    61: 'Brasilia'
+  };
+
+  ddds.addAll({90: 'Camaragibe'});
+
+  ddds.forEach((key, value) {
+    print("Key: ${key} Value: ${value}");
+  });
+
+  //* Null Assertion
+  String cidade = ddds[81]!;
+  print(cidade.toUpperCase());
+}
