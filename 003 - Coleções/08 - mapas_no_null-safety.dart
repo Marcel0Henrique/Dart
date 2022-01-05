0251f1b651d9ecc5da42e95561e5b8c1fc95faee
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-01-05 20:03:27 -0300

    Update 08 - mapas_no_null-safety.dart

--- 003 - Coleções/08 - mapas_no_null-safety.dart
+++ 003 - Coleções/08 - mapas_no_null-safety.dart
@@ -1,1 +1,7 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  Map<int, String?> ddds = {11: 'São paulo', 81: 'Pernambuco', 41: null};
+
+  Map<int, String>? ddds2;
+
+  Map<int, String?>? ddds3 = {11: 'São paulo', 81: 'Pernambuco', 41: null};
+}
