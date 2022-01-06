7210d4b3d43e8c98972deb2c9f3b8960a988d1ef
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-01-06 17:01:01 -0300

    Update 02 - exceptions_personalizadas.dart

--- 004 - Outros recursos essenciais/02 - exceptions_personalizadas.dart
+++ 004 - Outros recursos essenciais/02 - exceptions_personalizadas.dart
@@ -1,5 +1,16 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  try {
+    funcao(-10);
+  } catch (e) {
+    print(e);
+  }
+}
 
-void funcao(int x) {}
+void funcao(int x) {
+  if (x <= 0) {
+    throw parametroInvalido();
+  }
+  print(x);
+}
 
 class parametroInvalido implements Exception {}
