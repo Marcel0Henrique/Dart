cb932dc78b120cbe026cb6e52aa6220e2a6c277a
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-01-06 13:17:57 -0300

    Update 01 - exceptions_e_try_catch.dart

--- 004 - Outros recursos essenciais/01 - exceptions_e_try_catch.dart
+++ 004 - Outros recursos essenciais/01 - exceptions_e_try_catch.dart
@@ -1,8 +1,13 @@
 void main(List<String> args) {
   try {
     //? Tenta executar o codigo
-    int resultado = 100 ~/ 0;
+    int resultado = 100 ~/ 2;
     print(resultado);
+
+    double valor = double.parse('50.7');
+
+    print('');
+    print(valor);
   } on IntegerDivisionByZeroException {
     //? se o erro for IntegerDivisionByZeroException executa o comando abaixo
     print("Inteiro dividido por zero");
