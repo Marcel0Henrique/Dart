2f364802f0269f219172b993f0bca7be009484c6
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-02 20:20:23 -0300

    Create 04 - final_x_const.dart

---
+++ 004 - Outros recursos essenciais/04 - final_x_const.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  const String nome = "Marcelo";
+  const List<String> nomes = ['Marcelo', 'Henrique'];
+  const sobrenome = 'Henrique';
+
+  final int idade;
+  idade = 22;
+}
