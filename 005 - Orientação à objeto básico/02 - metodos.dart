d39db14b4cab893bb1be2bfbc12560f735c2ff58
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-04 19:35:49 -0300

    Update 02 - metodos.dart

--- 005 - Orientação à objeto básico/02 - metodos.dart
+++ 005 - Orientação à objeto básico/02 - metodos.dart
@@ -1,1 +1,45 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  Pessoa p1 = Pessoa();
+
+  p1.nome = 'Marcelo';
+  p1.idade = 22;
+
+  p1.aniversario();
+  print(p1.idade);
+
+  p1.casar();
+  print(p1.casado);
+
+  p1.trocarNome('Henrique');
+  print(p1.nome);
+
+  print(p1.mostrarIdade());
+}
+
+class Pessoa {
+  //*Atributos
+  String? nome;
+  int? idade;
+  bool casado = false;
+
+  //*Metodos
+
+  void aniversario() {
+    print("Parabéns $nome");
+    if (idade != null) {
+      idade = idade! + 1;
+    }
+  }
+
+  void casar() {
+    casado = true;
+  }
+
+  void trocarNome(String n) {
+    nome = n;
+  }
+
+  int? mostrarIdade() {
+    return idade;
+  }
+}
