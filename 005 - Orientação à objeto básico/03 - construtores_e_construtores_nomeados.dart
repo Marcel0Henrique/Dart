63d89e6817ceaf28784209da1f7121dee3af5e0f
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-04 20:49:47 -0300

    Update 03 - construtores_e_construtores_nomeados.dart

--- 005 - Orientação à objeto básico/03 - construtores_e_construtores_nomeados.dart
+++ 005 - Orientação à objeto básico/03 - construtores_e_construtores_nomeados.dart
@@ -1,17 +1,20 @@
 void main(List<String> args) {
   Carro gol = Carro(modelo: 'GOL', marca: 'volkswagen', ano: 2021);
+  Carro celta = Carro.usado(modelo: 'Celta', marca: 'Chevrolet', ano: 2002);
 }
 
 class Carro {
   //*Construtores
-  Carro({required String modelo, required String marca, required int ano}) {
-    this.modelo = modelo;
-    this.marca = marca;
-    this.ano = ano;
+  Carro({required this.modelo, required this.marca, required this.ano});
+
+  //*Contrutor nomeado
+  Carro.usado({required this.modelo, required this.marca, required this.ano}) {
+    novo = false;
   }
 
   //*Atributos
-  String? modelo;
-  String? marca;
-  int? ano;
+  String modelo;
+  String marca;
+  int ano;
+  bool novo = true;
 }
