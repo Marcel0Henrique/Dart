492d9bf6663da0fc1ba3eca9dd0174fa59c8e772
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-04 18:45:03 -0300

    update

--- 005 - Orientação à objeto básico/01 - classes_objetos_e_atributos.dart
+++ 005 - Orientação à objeto básico/01 - classes_objetos_e_atributos.dart
@@ -1,8 +1,22 @@
 void main(List<String> args) {
   //* Instanciando Objeto
   Pessoa p1 = Pessoa();
+  p1.nome = 'Marcelo';
+  p1.idade = 22;
+
+  print("Nome: ${p1.nome} \nIdade: ${p1.idade}\n");
+
+  //*instanciando um novo objeto
+  Pessoa p2 = Pessoa();
+  p2.nome = "Henrique";
+  p2.idade = 23;
+
+  print("Nome: ${p2.nome} \nIdade: ${p2.idade}\n");
 }
 
 //* Criando classe
 
-class Pessoa {}
+class Pessoa {
+  String? nome;
+  int? idade;
+}
