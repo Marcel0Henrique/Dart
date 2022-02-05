4e399a2a9dbedf12e1a23c46ad665e2d9886e67c
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-04 22:29:46 -0300

    Update 04 - getters_e_setters.dart

--- 005 - Orientação à objeto básico/04 - getters_e_setters.dart
+++ 005 - Orientação à objeto básico/04 - getters_e_setters.dart
@@ -1,1 +1,27 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  Humano h1 = Humano(nome: 'Marcelo', idade: 22);
+  h1.setDinheiro = 100;
+  print(h1.getDinheiro);
+}
+
+class Humano {
+  //*Construtor
+  Humano({required this.nome, required this.idade});
+
+  //*Atributos
+  String nome;
+  int idade;
+
+  //? colocando o _ na frente do atributo indentifica como um atributo privado
+  double? _dinheiro;
+
+  //*Getters
+  double? get getDinheiro {
+    return _dinheiro;
+  }
+
+  //*Setters
+  set setDinheiro(double valor) {
+    _dinheiro = valor;
+  }
+}
