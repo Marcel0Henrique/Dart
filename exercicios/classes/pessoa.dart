7f01ca1bf9e87738bed4b1c3193a4cd46f180072
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-04 22:57:38 -0300

    Update pessoa.dart

--- exercicios/classes/pessoa.dart
+++ exercicios/classes/pessoa.dart
@@ -1,7 +1,19 @@
 class Pessoa {
   //*Atributos
-  String? nome;
-  int? idade;
-  double? peso;
-  double? altura;
+  String? _nome;
+  int? _idade;
+  double? _peso;
+  double? _altura;
+
+  //*Getters
+  get nome => this._nome;
+  get idade => this._idade;
+  get peso => this._peso;
+  get altura => this._altura;
+
+  //*Setters
+  set nome(value) => this._nome = value;
+  set idade(value) => this._idade = value;
+  set peso(value) => this._peso = value;
+  set altura(value) => this._altura = value;
 }
