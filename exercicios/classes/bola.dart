d2f4cbfb9a275fa859a6d2c409dbed28cf44af4f
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-04 22:41:31 -0300

    Update bola.dart

--- exercicios/classes/bola.dart
+++ exercicios/classes/bola.dart
@@ -1,1 +1,20 @@
-class Bola {}
+class Bola {
+  //*Atributos
+  String? _cor;
+  String? _circunferencia;
+  String? _material;
+
+  //*Getters e Setters
+
+  set setCor(String cor) {
+    _cor = cor;
+  }
+
+  set setCircunferencia(String circun) {
+    _circunferencia = circun;
+  }
+
+  set setMaterial(String material) {
+    _material = material;
+  }
+}
