834236032d82455ea8bf7166c929f940ff1a36e0
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-05 17:40:22 -0300

    Update 07 - modificador_later.dart

--- 005 - Orientação à objeto básico/07 - modificador_later.dart
+++ 005 - Orientação à objeto básico/07 - modificador_later.dart
@@ -1,1 +1,18 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  Estudante e1 = Estudante(nome: 'Marcos', idade: 15);
+}
+
+class Estudante {
+  //*Construtor
+  Estudante({required String nome, required int idade}) {
+    _nome = nome;
+    _idade = idade;
+  }
+
+  //*Atributos
+  String? _nome;
+  int? _idade;
+
+  //? late indica que a variavel vai ser inicializada depois
+  late String cpf;
+}
