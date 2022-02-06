7254aa00c599e88563cede40c8278abe4daeb756
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-06 19:33:57 -0300

    Update 01 - heranca.dart

--- 006 - Heranca e Polimorfismo/01 - heranca.dart
+++ 006 - Heranca e Polimorfismo/01 - heranca.dart
@@ -1,1 +1,15 @@
+import 'dart:_internal';
+
 void main(List<String> args) {}
+
+class Animal {
+  String? _nome;
+  String? _especie;
+  int? _idade;
+
+  comer() => print('Comeu');
+
+  dormi() => print('Dormiu');
+}
+
+class Cachorro {}
