466a26512d4f218713e36272bea63853e1afcc6d
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-09 17:15:36 -0300

    Update 07 - outro_uso_para_interfaces.dart

--- 006 - Heranca e Polimorfismo/07 - outro_uso_para_interfaces.dart
+++ 006 - Heranca e Polimorfismo/07 - outro_uso_para_interfaces.dart
@@ -1,16 +1,31 @@
 void main(List<String> args) {
   Movel movel1 = Carro();
+  vendivel v1 = Geladeira();
+  print(v1.preco());
 }
 
+//*Interfaces
 abstract class Movel {
   void frente();
   void esquerda();
   void direita();
 }
 
-class Geladeira {}
+abstract class vendivel {
+  double preco();
+}
+
+//*Classes
+class Geladeira implements vendivel {
+  @override
+  double preco() {
+    return 2.0;
+  }
+}
 
 class Pessoa implements Movel {
+  String? nome;
+
   @override
   void direita() {}
 
@@ -21,7 +36,9 @@ class Pessoa implements Movel {
   void frente() {}
 }
 
-class Carro implements Movel {
+class Carro implements Movel, vendivel {
+  String? modelos;
+
   @override
   void direita() {}
 
@@ -30,4 +47,9 @@ class Carro implements Movel {
 
   @override
   void frente() {}
+
+  @override
+  double preco() {
+    return 2.0;
+  }
 }
