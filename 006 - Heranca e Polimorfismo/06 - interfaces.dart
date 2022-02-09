1c17ee39be401baf1a9e3013e27e5b18749e622e
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-09 14:01:26 -0300

    Update 06 - interfaces.dart

--- 006 - Heranca e Polimorfismo/06 - interfaces.dart
+++ 006 - Heranca e Polimorfismo/06 - interfaces.dart
@@ -1,1 +1,44 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  RepositorioPessoasRemote repo = RepositorioPessoasRemote();
+}
+
+//* interfaces
+abstract class RepositorioPessoas {
+  String ler(int id);
+  void adicionar(String nome);
+  void apagar(int id);
+}
+
+class RepositorioPessoasRemote implements RepositorioPessoas {
+  @override
+  void adicionar(String nome) {
+    // TODO: implement adicionar
+  }
+
+  @override
+  void apagar(int id) {
+    // TODO: implement apagar
+  }
+
+  @override
+  String ler(int id) {
+    return 'Henrique';
+  }
+}
+
+class RepositorioPessoasLocal implements RepositorioPessoas {
+  @override
+  void adicionar(String nome) {
+    // TODO: implement adicionar
+  }
+
+  @override
+  void apagar(int id) {
+    // TODO: implement apagar
+  }
+
+  @override
+  String ler(int id) {
+    return 'Marcelo';
+  }
+}
